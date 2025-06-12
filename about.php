9deb9d6cf66fa694160d840bdaec99ce2a0cd238
Azril Rozaqi Ar Rosyid <142295337+Azrilrozaqiarrosyid@users.noreply.github.com>
2025-06-13 05:53:17 +0700

    Update about.php

--- about.php
+++ about.php
@@ -22,10 +22,10 @@
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                            <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
                             <li class="nav-item"><a class="nav-link" href="form.php">Form</a></li>
                             <li class="nav-item"><a class="nav-link" href="data.php">Data</a></li>
-                            <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
+                            <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
                         </ul>
                     </div>
                 </div>
